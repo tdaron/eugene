bb98c8113bc70e2e2ae18c59aa8204645ff2c724
Théo Daron <theo@daron.be>
2026-05-21 10:22:47 +0200

    parsing of R instructions + immediate instructions

--- src/assembler.zig
+++ src/assembler.zig
@@ -25,24 +25,72 @@ fn consumeTrimmedUntil(input: *[]const u8, token: u8) ?[]const u8 {
     return null;
 }
 
-const opcode_map = std.StaticStringMap(vm.Opcode).initComptime(.{
-    .{ "add", vm.Opcode.add },
-    .{ "mov", vm.Opcode.mov },
-    .{ "load", vm.Opcode.load },
-    .{ "store", vm.Opcode.store },
-});
+fn parse_opcode(raw_opcode: []const u8) ?vm.Opcode {
+    inline for (@typeInfo(vm.Opcode).@"enum".fields) |field| {
+        if (std.mem.eql(u8, field.name, raw_opcode)) {
+            return @enumFromInt(field.value);
+        }
+    }
+    return null;
+}
+
+const AssemblerError = error{
+    MissingRegister,
+    MissingImmediate,
+    InvalidRegister,
+    InvalidImmediate,
+};
 
 const Assembler = struct {
-    fn parse_instruction(self: *Assembler, opcode: []const u8, line: *[]const u8) void {
+    inline fn parseRegister(line: *[]const u8) !u5 {
+        const op = consumeTrimmedUntil(line, ',') orelse return AssemblerError.MissingRegister;
+        if (op[0] != 's') return AssemblerError.InvalidRegister;
+        return std.fmt.parseInt(u5, op[1..], 10);
+    }
+    inline fn parseImmediate(line: *[]const u8) !u18 {
+        const op = consumeTrimmedUntil(line, ',') orelse return AssemblerError.MissingImmediate;
+        if (op[0] != '#') return AssemblerError.InvalidImmediate;
+        return std.fmt.parseInt(u18, op[1..], 10);
+    }
+    fn parseImmediateInstruction(opcode: vm.Opcode, line: *[]const u8) !u32 {
+        var instr = vm.ImmediateInstr{ .header = .{ .opcode = opcode } };
+
+        instr.reg = try parseRegister(line);
+        instr.immediate = try parseImmediate(line);
+        return @bitCast(instr);
+    }
+    fn parseRInstruction(opcode: vm.Opcode, line: *[]const u8) !u32 {
+        var instr = vm.RInstr{ .header = .{ .opcode = opcode } };
+
+        // get 3 operands
+
+        instr.r1 = try parseRegister(line);
+        instr.r2 = try parseRegister(line);
+        instr.dest = try parseRegister(line);
+
+        return @bitCast(instr);
+    }
+    fn parse_instruction(self: *Assembler, raw_opcode: []const u8, line: *[]const u8) void {
         _ = self;
 
-        if (opcode_map.get(opcode)) |op| {
-            std.debug.print("opcode: {any}\n", .{op});
-            while (consumeTrimmedUntil(line, ',')) |operand| {
-                std.debug.print("operand: {s}\n", .{operand});
+        if (parse_opcode(raw_opcode)) |opcode| {
+            std.debug.print("opcode: {any}\n", .{opcode});
+
+            switch (opcode) {
+                .add => {
+                    const instruction = parseRInstruction(opcode, line);
+                    std.debug.print("Instruction: {any}\n", .{instruction});
+                },
+                .mov => {
+                    const instruction = parseImmediateInstruction(opcode, line);
+                    std.debug.print("Instruction: {any}\n", .{instruction});
+                },
+                else => {
+                    std.debug.print("unknown opcode: {s}\n", .{raw_opcode});
+                },
             }
         } else {
-            std.debug.print("unknown opcode: {s}\n", .{opcode});
+            std.debug.print("unknown opcode: {s}\n", .{raw_opcode});
         }
     }
     fn parse_line(self: *Assembler, line: *[]const u8) void {
@@ -63,8 +111,8 @@ const Assembler = struct {
 test "assembler basic" {
     var assembler = Assembler{};
     assembler.assemble(
-        \\add s1, s2, s3
-        \\mov s3, s1
-        \\add s1, s1, #5
+        \\add s2, s3, s7
+        \\mov s3, #9223
+        \\add s1, s1, s10
     );
 }
