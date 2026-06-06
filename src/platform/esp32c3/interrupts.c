49f67eb08612e47ea84a71fe6038991d7665e867
Théo Daron <theo@daron.be>
2026-06-06 22:28:06 +0200

    esp32c3: removed unused function

--- src/platform/esp32c3/interrupts.c
+++ src/platform/esp32c3/interrupts.c
@@ -15,26 +15,4 @@ void route_interrupt(volatile u32 *interrupt, u32 trap_number, u32 priority) {
 
 }
 
-void route_timg0_t0_interrupt(void) {
-  csr_disable_interrupts();
-
-  int CPU_INT_TG0_T0 = 1;
-
-  /* Route peripheral source TG_T0_INT to CPU interrupt 1 */
-  INTR_TG_T0_MAP = CPU_INT_TG0_T0;
-
-  /* Level-triggered CPU interrupt */
-  INTR_CPU_TYPE &= ~(1u << CPU_INT_TG0_T0);
-
-  /* Priority must be nonzero */
-  INTR_CPU_PRI(CPU_INT_TG0_T0) = 1;
-
-  /* Allow priority 1 interrupts through */
-  INTR_CPU_THRESH = 0;
-
-  /* Enable CPU interrupt 1 */
-  INTR_CPU_ENABLE |= (1u << CPU_INT_TG0_T0);
-
-  csr_enable_interrupts();
-}
 #endif
